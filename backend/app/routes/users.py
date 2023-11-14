4df36978b78d31999ad4aabe2aa8359fc9582168
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2023-11-14 14:11:36 -0800

    add mongodb

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -2,30 +2,30 @@ from fastapi import APIRoute
 
 router = APIRoute(prefix="/users")
 
-@router.get("/")
+@router.get("/", response_description="List All Users")
 async def get_users():
     return ["juliet", "John"]
 
-@router.get("/{user_id}")
+@router.get("/{user_id}", response_description="Get One User")
 async def get_user(user_id):
     return {"user_id": user_id}
 
-@router.get("/{user_id}/chats")
+@router.get("/{user_id}/chats", response_description="Get chats for a user")
 async def get_user_chats():
     return "The chats for a particular user"
 
-@router.post("/{user_id}/chats")
+@router.post("/{user_id}/chats", response_description="Create new chat")
 async def create_new_chat():
     return "new chat"
 
-@router.get("/{user_id}/chats/{chat_id}")
+@router.get("/{user_id}/chats/{chat_id}", response_description="get one chat")
 async def get_one_chat():
     return "new chat"
 
-@router.put("/{user_id}/chats/{chat_id}")
+@router.put("/{user_id}/chats/{chat_id}", response_description="Update one chat")
 async def update_one_chat():
-    return "new chat"
+    return "update chat"
 
-@router.delete("/{user_id}/chats/{chat_id}")
+@router.delete("/{user_id}/chats/{chat_id}", response_description="Delete one chat")
 async def delete_one_chat():
-    return "new chat"
+    return "delete chat"
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.routes import users
+from motor.motor_asyncio import AsyncIOMotorClient
+import settings
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
 
 app.include_router(users.router, prefix="/api/v1")
 
