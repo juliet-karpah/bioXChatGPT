c694b2c0b39a95a887a70c62ce0e2f7bb352dc38
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2023-11-14 20:10:23 -0800

    Update chats.py

--- backend/app/models/chats.py
+++ backend/app/models/chats.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 from typing import NamedTuple, List
 
 class ChatUnit(NamedTuple):
+    id: str
     question: str
     answer: List[str]
 
 class Chat(BaseModel):
     user_id: str
     title: str
-    sections: List[ChatUnit]
+    sections: List[ChatUnit.id]
